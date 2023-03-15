6bcf55a652a068c1759554af82205b00e7371a36
bbilich <bbilic02@fesb.hr>
2023-03-15 19:12:55 +0100

    Add console line

--- NoviLAb1/Program.cs
+++ NoviLAb1/Program.cs
@@ -6,7 +6,7 @@ namespace NoviLAb1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
